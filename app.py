70ea83da963f7e8a439b926f3ff5695e66fa2b1a
Ayush Rusia <ayushrusia21@gmail.com>
2026-06-16 15:18:08 +0000

    Updated application message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"<h1>Hello from Python Application!</h1>")
+        self.wfile.write(b"<h1>return "Version 2 - Git Working!"</h1>")
 
 server = HTTPServer(("0.0.0.0", 5000), Handler)
 print("Server running on port 5000...")
