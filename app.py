79d656ed305fe9e945d97ba212d90f10a44aa4e9
Ayush Rusia <ayushrusia21@gmail.com>
2026-06-16 15:04:25 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello from Python Application!</h1>")
+
+server = HTTPServer(("0.0.0.0", 5000), Handler)
+print("Server running on port 5000...")
+server.serve_forever()
